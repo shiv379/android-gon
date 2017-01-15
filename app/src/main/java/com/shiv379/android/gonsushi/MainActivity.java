d8d7036674389e8c8a67ff17088176b8d32b6908
Toby Berriman <shiv379@gmail.com>
2017-01-15 14:53:41 +0000

    Remove map code from master

--- app/src/main/java/com/shiv379/android/gonsushi/MainActivity.java
+++ app/src/main/java/com/shiv379/android/gonsushi/MainActivity.java
@@ -8,24 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
         setContentView(R.layout.activity_main);
     }
 
-    public class MapsMarkerActivity extends AppCompatActivity
-            implements OnMapReadyCallback {
-        // Include the OnCreate() method here too, as described above.
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            // Add a marker in Sydney, Australia,
-            // and move the map's camera to the same location.
-            LatLng sydney = new LatLng(-33.852, 151.211);
-            googleMap.addMarker(new MarkerOptions().position(sydney)
-                    .title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-            }
-    }
 }
