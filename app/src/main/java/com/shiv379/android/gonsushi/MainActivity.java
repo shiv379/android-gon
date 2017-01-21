1e4bf0d7cd4d94a52c16cfd6dda6e45e3133e15a
Toby Berriman <shiv379@gmail.com>
2017-01-21 22:48:53 +0000

    FEAT: Added fade effect to quote

--- app/src/main/java/com/shiv379/android/gonsushi/MainActivity.java
+++ app/src/main/java/com/shiv379/android/gonsushi/MainActivity.java
@@ -4,12 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.TextView;
 import java.util.Random;
 import java.util.ArrayList;
 
 import java.util.Timer;
-import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 private Random randomGenerator;
@@ -37,19 +38,66 @@ private Timer timer = new Timer();
         quoteList.add("“Beautiful food and wonderful staff!”");
         quoteList.add("“A hidden diamond in the rough”");
         quoteList.add("“Excellent taste! I love it!”");
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView t = (TextView) findViewById(R.id.quote);
-                        t.setText(quoteList.get(randomGenerator.nextInt(quoteList.size())));
-                    }});
-            }
-        }, 5000, 5000);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView t = (TextView) findViewById(R.id.quote);
+        final Animation out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(2000);
+        out.setStartOffset(5000);
+
+        final Animation in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(2000);
+
+        out.setAnimationListener(new Animation.AnimationListener(){
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation2 when animation1 ends (continue)
+                t.setText(quoteList.get(randomGenerator.nextInt(quoteList.size())));
+                t.startAnimation(in);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+        });
+
+        in.setAnimationListener(new Animation.AnimationListener(){
+
+            @Override
+            public void onAnimationEnd(Animation arg0) {
+                // start animation1 when animation2 ends (repeat)
+                t.startAnimation(out);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onAnimationStart(Animation arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+        });
+
+
+        t.startAnimation(out);
+
     }
     public void goSecondView(View view) {
         Intent intent = new Intent(this, Main2Activity.class);
